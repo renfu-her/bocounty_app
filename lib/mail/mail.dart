977df695749a2820a3a14608d3a7ac7a57f56a1b
Patrick Her <renfu.her@gmail.com>
2023-12-04 15:34:00 +0800

    update mail.dart

--- lib/mail/mail.dart
+++ lib/mail/mail.dart
@@ -33,13 +33,14 @@ class _MailPageState extends State<MailPage> {
     _focusNode.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
     return GestureDetector(
-        onTap: (){
+        onTap: () {
           FocusScope.of(context).unfocus();
           setState(() {
             isMenuOpen = true;
@@ -63,78 +64,76 @@ class _MailPageState extends State<MailPage> {
                       children: <Widget>[
                         Expanded(
                             child: Align(
-                              alignment: Alignment.topLeft,
-                              child: Container(
-                                padding: const EdgeInsets.only(left: 25,top: 25),
-                                height: screenHeight*0.08,
-                                width: screenHeight*0.08,
-                                child: InkWell(
-                                    onTap: () {
-                                      Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => const HomePage()),
-                                      );
-                                      print('bakeButton click');
-                                    },
-                                    child: Image.asset('assets/images/back.png')
-                                ),
-                              ),
-                            )
-                        ),
+                          alignment: Alignment.topLeft,
+                          child: Container(
+                            padding: const EdgeInsets.only(left: 25, top: 25),
+                            height: screenHeight * 0.08,
+                            width: screenHeight * 0.08,
+                            child: InkWell(
+                                onTap: () {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const HomePage()),
+                                  );
+                                  print('bakeButton click');
+                                },
+                                child: Image.asset('assets/images/back.png')),
+                          ),
+                        )),
                         Center(
                             child: Column(
-                              children: [
-                                Container(
-                                  alignment: Alignment.center,
-                                  width: screenWidth*0.6,
-                                  child: InkWell(
-                                      onTap: () {
-                                        if(isMenuOpen==true){
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(builder: (context) => const DealPage()),
-                                          );
-                                          print('deal click');
-                                        }else{
-                                          setState(() {
-                                            isMenuOpen=true;
-                                          });
-                                        }
-                                      },
-                                      child: Image.asset('assets/images/mail/deal.png')
-                                  ),
-                                ),
-                                SizedBox(height: screenHeight*0.1,),
-                                Container(
-                                  alignment: Alignment.center,
-                                  width: screenWidth*0.6,
-                                  child: InkWell(
-                                      onTap: () {
-                                        if(isMenuOpen==true){
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(builder: (context) => const NotifyPage()),
-                                          );
-                                          print('notify click');
-                                        }else{
-                                          setState(() {
-                                            isMenuOpen=true;
-                                          });
-                                        }
-                                      },
-                                      child: Image.asset('assets/images/mail/notify.png')
-                                  ),
-                                ),
-                                SizedBox(height: screenHeight*0.15,)
-                              ],
-                            )
-                        )
+                          children: [
+                            Container(
+                              alignment: Alignment.center,
+                              width: screenWidth * 0.6,
+                              child: InkWell(
+                                  onTap: () {
+                                    if (isMenuOpen == true) {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const DealPage()),
+                                      );
+                                      print('deal click');
+                                    } else {
+                                      setState(() {
+                                        isMenuOpen = true;
+                                      });
+                                    }
+                                  },
+                                  child: Image.asset(
+                                      'assets/images/mail/deal.png')),
+                            ),
+                            SizedBox(
+                              height: screenHeight * 1 / 2,
+                            ),
+                            // Container(
+                            //   alignment: Alignment.center,
+                            //   width: screenWidth*0.6,
+                            //   child: InkWell(
+                            //       onTap: () {
+                            //         if(isMenuOpen==true){
+                            //           Navigator.push(
+                            //             context,
+                            //             MaterialPageRoute(builder: (context) => const NotifyPage()),
+                            //           );
+                            //           print('notify click');
+                            //         }else{
+                            //           setState(() {
+                            //             isMenuOpen=true;
+                            //           });
+                            //         }
+                            //       },
+                            //       child: Image.asset('assets/images/mail/notify.png')
+                            //   ),
+                            // ),
+                          ],
+                        ))
                       ],
-                    )
-                ),
+                    )),
               ],
-            )
-        )
-    );
+            )));
   }
 }
