1903bbbb6e314872605f0c39ed8b068128724ce9
Patrick Her <renfu.her@gmail.com>
2023-12-05 21:39:26 +0800

    update shop.dart

--- lib/shop.dart
+++ lib/shop.dart
@@ -134,7 +134,7 @@ class _ShopPageState extends State<ShopPage> {
                       image: DecorationImage(
                         image: AssetImage('assets/images/shop.png'),
                         fit: BoxFit.cover,
-                        alignment: Alignment.topCenter,
+                        // alignment: Alignment.topCenter,
                       ),
                     ),
                     child: Column(
